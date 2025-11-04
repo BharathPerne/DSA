be2baed253f519af6bf76b5c5e30d678a95a1058
Bharath <149702264+BharathPerne@users.noreply.github.com>
2025-11-04 20:23:20 +0530

    Time: 26 ms (20.77%) | Memory: 38.3 MB (11.50%) - LeetSync

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        int n = nums.size();
+        vector<int> result;
+        
+        for (int i = 0; i <= n - k; i++) {
+            unordered_map<int, int> freq;
+            for (int j = i; j < i + k; j++) {
+                freq[nums[j]]++;
+            }
+            
+            vector<pair<int, int>> elements;
+            for (auto& [val, count] : freq) {
+                elements.push_back({count, val});
+            }
+            
+            sort(elements.begin(), elements.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+                if (a.first != b.first) return a.first > b.first;
+                return a.second > b.second;
+            });
+            
+            vector<int> topValues;
+            for (int idx = 0; idx < x && idx < elements.size(); idx++) {
+                topValues.push_back(elements[idx].second);
+            }
+            
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                if (find(topValues.begin(), topValues.end(), nums[j]) != topValues.end()) {
+                    sum += nums[j];
+                }
+            }
+            
+            result.push_back(sum);
+        }
+        
+        return result;
+    }
+};
