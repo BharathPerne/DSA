adfe94ac45e28bf61efa540aa2c80972ec695b39
Bharath <149702264+BharathPerne@users.noreply.github.com>
2025-11-23 06:56:53 +0530

    Time: 11 ms (65.34%) | Memory: 28.9 MB (84.90%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,33 +1,68 @@
+// // #include <vector>
+// // #include <algorithm>
+// // using namespace std;
+
+// // class Solution {
+// // public:
+// //     int solve(vector<int>& nums, int index, int sum) {
+// //         if (index == nums.size()) {
+// //             return (sum % 3 == 0) ? sum : 0;
+// //         }
+        
+// //         int include = solve(nums, index + 1, sum + nums[index]);
+// //         int exclude = solve(nums, index + 1, sum);
+        
+// //         if (include % 3 == 0 && exclude % 3 == 0) {
+// //             return max(include, exclude);
+// //         } else if (include % 3 == 0) {
+// //             return include;
+// //         } else if (exclude % 3 == 0) {
+// //             return exclude;
+// //         } else {
+// //             return 0;
+// //         }
+// //     }
+    
+// //     int maxSumDivThree(vector<int>& nums) {
+// //         return solve(nums, 0, 0);
+// //     }
+// // };
+
 // #include <vector>
 // #include <algorithm>
 // using namespace std;
 
 // class Solution {
 // public:
-//     int solve(vector<int>& nums, int index, int sum) {
-//         if (index == nums.size()) {
-//             return (sum % 3 == 0) ? sum : 0;
-//         }
-        
-//         int include = solve(nums, index + 1, sum + nums[index]);
-//         int exclude = solve(nums, index + 1, sum);
-        
-//         if (include % 3 == 0 && exclude % 3 == 0) {
-//             return max(include, exclude);
-//         } else if (include % 3 == 0) {
-//             return include;
-//         } else if (exclude % 3 == 0) {
-//             return exclude;
-//         } else {
-//             return 0;
-//         }
-//     }
-    
 //     int maxSumDivThree(vector<int>& nums) {
-//         return solve(nums, 0, 0);
+//         int n = nums.size();
+        
+//         vector<vector<int>> dp(n + 1, vector<int>(3, -1));
+        
+//         dp[0][0] = 0;
+//         dp[0][1] = -1;  // Not achievable
+//         dp[0][2] = -1;  // Not achievable
+        
+//         for (int i = 1; i <= n; i++) {
+//             int num = nums[i - 1];
+//             int rem = num % 3;
+            
+//             for (int j = 0; j < 3; j++) {
+//                 dp[i][j] = dp[i - 1][j];
+                
+//                 int prevRem = (j - rem + 3) % 3;
+//                 if (dp[i - 1][prevRem] != -1) {
+//                     int newSum = dp[i - 1][prevRem] + num;
+//                     if (dp[i][j] == -1 || newSum > dp[i][j]) {
+//                         dp[i][j] = newSum;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return dp[n][0] != -1 ? dp[n][0] : 0;
 //     }
 // };
-
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -37,29 +72,38 @@ public:
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size();
         
-        vector<vector<int>> dp(n + 1, vector<int>(3, -1));
+        // Use only two arrays: prev and curr
+        vector<int> prev(3, -1);
+        vector<int> curr(3, -1);
         
-        dp[0][0] = 0;
-        dp[0][1] = -1;  // Not achievable
-        dp[0][2] = -1;  // Not achievable
+        // Base case: with 0 elements, sum 0 has remainder 0
+        prev[0] = 0;
+        prev[1] = -1;  
+        prev[2] = -1; 
         
-        for (int i = 1; i <= n; i++) {
-            int num = nums[i - 1];
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
             int rem = num % 3;
             
+            // Initialize curr with prev values (exclude current element case)
+            curr = prev;
+            
             for (int j = 0; j < 3; j++) {
-                dp[i][j] = dp[i - 1][j];
-                
-                int prevRem = (j - rem + 3) % 3;
-                if (dp[i - 1][prevRem] != -1) {
-                    int newSum = dp[i - 1][prevRem] + num;
-                    if (dp[i][j] == -1 || newSum > dp[i][j]) {
-                        dp[i][j] = newSum;
+                if (prev[j] != -1) {
+                    int newRem = (j + rem) % 3;
+                    int newSum = prev[j] + num;
+                    
+                    // Update curr if we found a better sum for this remainder
+                    if (curr[newRem] == -1 || newSum > curr[newRem]) {
+                        curr[newRem] = newSum;
                     }
                 }
             }
+            
+            // Move to next iteration: curr becomes prev
+            prev = curr;
         }
         
-        return dp[n][0] != -1 ? dp[n][0] : 0;
+        return prev[0] != -1 ? prev[0] : 0;
     }
 };
