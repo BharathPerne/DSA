46f1a5ea7338c17002fca9599939bc11d40ef9ef
Bharath <149702264+BharathPerne@users.noreply.github.com>
2025-11-29 06:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (69.81%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -4,8 +4,8 @@ public:
         int sum=0;
         for(int i=0;i<nums.size();i++)
         {
-            sum=(sum+nums[i])%k;
+            sum+=nums[i];
         }
-        return sum;
+        return sum%k;
     }
 };
