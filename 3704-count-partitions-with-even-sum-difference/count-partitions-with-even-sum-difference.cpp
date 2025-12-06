4c0164893d3d67d9169d4b40a348b2141078e0c4
Bharath <149702264+BharathPerne@users.noreply.github.com>
2025-12-06 09:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (53.57%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int countPartitions(vector<int>& nums) {
+        //In this one two approach are there one is we can take the int and do the operation and also we can take the bool data type for the memory optimization in this to find out the odd numbers we are fliping the variable then we are checking the odd or even.
         bool flag=0;
         for(int i=0;i<nums.size();i++)
         {
