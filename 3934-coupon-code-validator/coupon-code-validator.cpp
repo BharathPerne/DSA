1f07288acf0736dac91161b5b421f5ed9d3665d1
Bharath <149702264+BharathPerne@users.noreply.github.com>
2025-12-13 10:38:52 +0530

    Time: 3 ms (81.55%) | Memory: 69.6 MB (46.35%) - LeetSync

--- 3934-coupon-code-validator/coupon-code-validator.cpp
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -1,6 +1,70 @@
+// class Solution {
+// public:
+// bool isvalid(string str)
+// {
+//     if(str.size()<=0)
+//     {
+//         return false;
+//     }
+//     for(int i=0;i<str.size();i++)
+//     {
+//         if ( ('a' <= str[i] && str[i] <= 'z') || ('A' <= str[i] && str[i] <= 'Z') ||('0'<=str[i]&&str[i]<='9')||str[i] == '_' )
+//         {
+//             continue;
+//         }
+//         else
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+//     vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+//         vector<string> electronics;
+//         vector<string> grocery;
+//         vector<string> pharmacy;
+//         vector<string> restaurant;
+//         for(int i=0;i<code.size();i++)
+//         {
+//             if(isvalid(code[i]))
+//             {
+//                 if(isActive[i])
+//                 {
+//                     if(businessLine[i]=="restaurant")
+//                     {
+//                         restaurant.push_back(code[i]);
+//                     }
+//                     else if(businessLine[i]=="grocery")
+//                     {
+//                         grocery.push_back(code[i]);
+//                     }
+//                     else if(businessLine[i]=="pharmacy")
+//                     {
+//                         pharmacy.push_back(code[i]);
+//                     }
+//                     else if(businessLine[i]=="electronics")
+//                     {
+//                         electronics.push_back(code[i]);
+//                     }
+//                 }
+//             }
+//         }
+//         vector<string> result;
+//         sort(electronics.begin(), electronics.end());
+//     sort(grocery.begin(), grocery.end());
+//     sort(pharmacy.begin(), pharmacy.end());
+//     sort(restaurant.begin(), restaurant.end());
+//     result.insert(result.end(), electronics.begin(), electronics.end());
+//     result.insert(result.end(), grocery.begin(), grocery.end());
+//     result.insert(result.end(), pharmacy.begin(), pharmacy.end());
+//     result.insert(result.end(), restaurant.begin(), restaurant.end());
+//     return result;
+//     }
+// };
+
 class Solution {
 public:
-bool isvalid(string str)
+bool isValid(string str)
 {
     if(str.size()<=0)
     {
@@ -19,45 +83,41 @@ bool isvalid(string str)
     }
     return true;
 }
-    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
-        vector<string> electronics;
-        vector<string> grocery;
-        vector<string> pharmacy;
-        vector<string> restaurant;
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive)
+    {
+        map<string, priority_queue<string, vector<string>, greater<string>>> categoryHeaps;
         for(int i=0;i<code.size();i++)
         {
-            if(isvalid(code[i]))
+            if(isValid(code[i]))
             {
                 if(isActive[i])
                 {
                     if(businessLine[i]=="restaurant")
                     {
-                        restaurant.push_back(code[i]);
+                        categoryHeaps["restaurant"].push(code[i]);
                     }
                     else if(businessLine[i]=="grocery")
                     {
-                        grocery.push_back(code[i]);
+                        categoryHeaps["grocery"].push(code[i]);
                     }
                     else if(businessLine[i]=="pharmacy")
                     {
-                        pharmacy.push_back(code[i]);
+                        categoryHeaps["pharmacy"].push(code[i]);
                     }
                     else if(businessLine[i]=="electronics")
                     {
-                        electronics.push_back(code[i]);
+                        categoryHeaps["electronics"].push(code[i]);
                     }
                 }
             }
         }
         vector<string> result;
-        sort(electronics.begin(), electronics.end());
-    sort(grocery.begin(), grocery.end());
-    sort(pharmacy.begin(), pharmacy.end());
-    sort(restaurant.begin(), restaurant.end());
-    result.insert(result.end(), electronics.begin(), electronics.end());
-    result.insert(result.end(), grocery.begin(), grocery.end());
-    result.insert(result.end(), pharmacy.begin(), pharmacy.end());
-    result.insert(result.end(), restaurant.begin(), restaurant.end());
-    return result;
+        for (auto& [category, heap] : categoryHeaps) {
+        while (!heap.empty()) {
+            result.push_back(heap.top());
+            heap.pop();
+        }
+    }
+     return result;
     }
 };
