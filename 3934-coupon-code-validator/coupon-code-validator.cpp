bf2e16562011e41e00d11cb87efa0346c0ecb8a0
Bharath <149702264+BharathPerne@users.noreply.github.com>
2025-12-13 09:46:10 +0530

    Time: 16 ms (11.59%) | Memory: 69.3 MB (56.22%) - LeetSync

---
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+bool isvalid(string str)
+{
+    if(str.size()<=0)
+    {
+        return false;
+    }
+    for(int i=0;i<str.size();i++)
+    {
+        if ( ('a' <= str[i] && str[i] <= 'z') || ('A' <= str[i] && str[i] <= 'Z') ||('0'<=str[i]&&str[i]<='9')||str[i] == '_' )
+        {
+            continue;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        vector<string> electronics;
+        vector<string> grocery;
+        vector<string> pharmacy;
+        vector<string> restaurant;
+        for(int i=0;i<code.size();i++)
+        {
+            if(isvalid(code[i]))
+            {
+                if(isActive[i])
+                {
+                    if(businessLine[i]=="restaurant")
+                    {
+                        restaurant.push_back(code[i]);
+                    }
+                    else if(businessLine[i]=="grocery")
+                    {
+                        grocery.push_back(code[i]);
+                    }
+                    else if(businessLine[i]=="pharmacy")
+                    {
+                        pharmacy.push_back(code[i]);
+                    }
+                    else if(businessLine[i]=="electronics")
+                    {
+                        electronics.push_back(code[i]);
+                    }
+                }
+            }
+        }
+        vector<string> result;
+        sort(electronics.begin(), electronics.end());
+    sort(grocery.begin(), grocery.end());
+    sort(pharmacy.begin(), pharmacy.end());
+    sort(restaurant.begin(), restaurant.end());
+    result.insert(result.end(), electronics.begin(), electronics.end());
+    result.insert(result.end(), grocery.begin(), grocery.end());
+    result.insert(result.end(), pharmacy.begin(), pharmacy.end());
+    result.insert(result.end(), restaurant.begin(), restaurant.end());
+    return result;
+    }
+};
